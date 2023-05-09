2c518403f3b8177a2a2723d22c81e6c7753bb567
developer <guzman.perlaedith@outlook.com>
2023-05-09 13:53:54 -0400

    first commit

---
+++ NotesMAUI/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace NotesMAUI;
+
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
+}
+
